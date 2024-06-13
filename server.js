52ae2e550ea76cd7412033161cc49b81f81492f1
Tereshchenko <an.drey@email.cz>
2024-06-13 12:20:06 +0200

    add new port

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+// const PORT = 3000;
+
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
