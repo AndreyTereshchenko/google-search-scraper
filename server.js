d387ceeb30c59f3eac44c4ba1d1360f84332fe5b
Tereshchenko <an.drey@email.cz>
2024-06-13 11:55:35 +0200

    change app.use

--- server.js
+++ server.js
@@ -53,6 +53,16 @@ async function scrapeGoogleSearch(query) {
     return results;
 }
 
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
